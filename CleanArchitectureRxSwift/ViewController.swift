37d4c9b7f2ea17416f9f8502a672aaa278833234
psychehose <psychehose@gmail.com>
2021-11-23 08:41:42 +0900

    Init and Pod

---
+++ CleanArchitectureRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitectureRxSwift
+//
+//  Created by 호세김 on 2021/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .white
+	}
+
+
+}
+
